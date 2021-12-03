0131d38c4e3f8f06310a2eea6be72439eab0678c
mikhailandreev <andreemic@gmail.com>
2021-12-03 20:32:08 +0100

    code editor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
+function App() { 
+  const onChange = console.log;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AceEditor
+        mode="javascript"
+        theme="github"
+        onChange={onChange}
+        name="graphcode"
+        editorProps={{}}
+      />
     </div>
   );
 }
