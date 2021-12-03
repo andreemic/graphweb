f7af2c33fd3e76b25d86c526e36c6161a925037a
Moritz-Friedemann <mofriedemann@web.de>
2021-12-03 20:50:32 +0100

    execute user code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from "react"
 import AceEditor from "react-ace";
 
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/theme-github";
-function App() { 
-  const onChange = console.log;
+function App() {
+  const [codeVal, setCodeVal] = useState(`function onLoad(editor) {
+    console.log("i've loaded");
+  }`);
+  const onChange = (val, e) => setCodeVal(val)
+  const runUserCode = () => {
+    eval(codeVal)
+    console.log(codeVal)
+  };
+
   return (
-    <div className="App">
+    <div>
       <AceEditor
+        placeholder="Placeholder Text"
         mode="javascript"
         theme="github"
+        name="graph-code"
         onChange={onChange}
-        name="graphcode"
-        editorProps={{}}
-      />
+        fontSize={14}
+        showPrintMargin={true}
+        showGutter={true}
+        highlightActiveLine={true}
+        value={codeVal}
+        setOptions={{
+          enableBasicAutocompletion: false,
+          enableLiveAutocompletion: false,
+          enableSnippets: false,
+          showLineNumbers: true,
+          tabSize: 2,
+        }} />
+
+      <button onClick={runUserCode}>Run</button>
+
     </div>
+
   );
 }
 
-export default App;
+function printTest() {
+  console.log("Test");
+}
+
+export default App
