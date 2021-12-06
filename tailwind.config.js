83f1fe9c3e60b2c40667bf6470ed64fc13f31c62
mikhailandreev <andreemic@gmail.com>
2021-12-06 09:30:11 +0100

    style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      amber: {
-        400: "#FFBF69",
-        500: "#FF9F1C"
+    extend: {
+      colors: {
+        amber: {
+          400: "#FFBF69",
+          500: "#FF9F1C"
+        }
       }
-    }
+    },
+    
   },
   variants: {
     extend: {},
